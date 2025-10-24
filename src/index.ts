f110dff559867e342b499c23c0232038ac526771
Burgaz <henihazbay@gmail.com>
2025-10-24 14:26:07 +0100

    feat: export normalization and Xtream helpers from library entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export * from './types.js';
 export { parsePlaylist } from './parser.js';
+export { normalizeEntry, normalizePlaylist } from './normalize.js';
+export { isXtreamUrl, parseXtream, makeXtreamCredentials, buildXtreamM3uUrl, buildXtreamCatchupUrl } from './xtream.js';
